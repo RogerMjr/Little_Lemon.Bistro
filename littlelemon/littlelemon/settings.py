ed8bb126df3f035bd67e069f0fb957c32113616a
RogerMjr <rogeriomessiasjr62@gmail.com>
2023-03-22 07:16:13 -0400

    .

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'restaurant'
+    'rest_framework.authtoken',
+    'restaurant',
+    'djoser'
+    
+
 ]
 
 MIDDLEWARE = [
@@ -130,3 +134,24 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+   
+    
+}
+
+DJOSER = {
+    "USER_ID_FILED" : "username",
+}
+
+AUTH_PASSWORD_VALIDATORS = []
